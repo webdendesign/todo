3d0b6f1fe79dd20fc488bff470e9e5e5451502c0
Den <fabri.din@yandex.ru>
2019-11-05 01:21:38 +0500

    event end components

---
+++ src/components/todo-list/todo-list.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import TodoListItem from "../todo-list-item";
+import "./todo-list.css";
+
+const TodoList = ({ todos }) => {
+  const elements = todos.map(item => {
+    const { id, ...itemProps } = item;
+
+    return (
+      <li key={item.id} className="list-group-item">
+        <TodoListItem {...itemProps} />
+      </li>
+    );
+  });
+
+  return <ul className="list-group todo-list">{elements}</ul>;
+};
+
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import TodoList from "./components/todo-list";
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
 
-const App = () => {
-  return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList />
-    </div>
-  );
-};
+import App from "./components/app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
